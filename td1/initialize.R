2f481e51af944a99187d6b174193157df19c5293
Karthik Ram <karthik.ram@gmail.com>
2012-06-11 14:20:10 -0700

    Reducing parameter combinations for now.

--- td1/initialize.R
+++ td1/initialize.R
@@ -1,29 +1,50 @@
 # Initialize basic libraries and functions
 rm(list = ls())
+message("Loading libraries \n")
 library(plyr)
 library(varDev2)
-library(lubridate)
+suppressPackageStartupMessages(library(lubridate))
 library(stringr)
-library(gridExtra)
 library(multicore)
-library(data.table)
-library(R.utils)
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(R.utils))
+message("Loading functions \n")
 source("tfunctions.R")
 
+# # No need to run this re run expensive operation unless necessary
+# message("Generating various tradeoff combinations \n")
+# # Basic tradeoff, adult survival, and fecundity
+# a1 <- seq(0.9, 0.1, by = -0.1)
+# b1 <- seq(-0.9, -0.1, by = 0.1)
+# a <- 0.9
+# b <- -0.8
+# sA <- 0.4
+# sA2 <- seq(0.1, 0.9, by = 0.1)
+# Fec <- 2
+# basic_params <- param_combs(a, b, sA, Fec)
 
-# Basic tradeoff, adult survival, and fecundity
-a <- seq(0.9, 0.1, by = -0.1)
-b <- seq(-0.9, -0.1, by = 0.1)
-sA <- seq(0.1, 0.9, by = 0.1)
-Fec <- 1:10
-basic_params <- param_combs(a, b, sA, Fec)
+# message("\n juvparams")
+# # Next, add in a range of cv values
+# # cv is defined as sqrt(juvshape)/juvshape
+# juvshape <- seq(1,50, by = 5)
+# vd_params <- param_combs_jg(a, b, sA, Fec, juvshape)
 
-# Next, add in a range of cv values
-# cv is defined as sqrt(juvshape)/juvshape
-juvshape <- seq(1,50, by = 2)
-vd_params <- param_combs_jg(a, b, sA, Fec, juvshape)
 
-# Now add in a correlation
-corr <- seq(0.1, 0.9, by = 0.1)
-corr_params <- param_combs_corr(a, b, sA, Fec, juvshape, corr)
+# message("\n Finally, corr_params")
+# # Now add in a correlation
+# corr <- seq(0.1, 0.9, by = 0.25)
+# corr_params <- param_combs_corr(a, b, sA, Fec, juvshape, corr)
+
+# # basic_params = 1215
+# # vd_params = 12150
+# # corr_params = 48600
+
+# message ("Now saving the params to disk \n")
+# save(basic_params, file="basic_params.rda")
+# save(vd_params, file="vd_params.rda")
+# save(corr_params, file="corr_params.rda")
+# paths=('.')
+# library(tools)
+# checkRdaFiles(paths)
+# resaveRdaFiles(paths, compress = "auto")
 
