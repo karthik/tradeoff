5e92c30a916d3fdc4bc607bb0a65e77961c216b1
Karthik Ram <karthik.ram@gmail.com>
2012-09-23 18:36:39 -0700

    added script to generate various formats of the manuscript

---
+++ manuscript/docs.sh
@@ -0,0 +1,6 @@
+pandoc -s -S --bibliography tradeoff.bib --mathjax --csl the-american-naturalist.csl tradeoffms.md -o ms.pdf
+pandoc -s -S --bibliography tradeoff.bib --mathjax --csl the-american-naturalist.csl tradeoffms.md -o ms.docx
+pandoc -s -S --bibliography tradeoff.bib --mathjax --csl the-american-naturalist.csl tradeoffms.md -o ms.tex
+pandoc -s -S --bibliography tradeoff.bib --mathjax --csl the-american-naturalist.csl tradeoffms.md -o ms.txt
+
+
