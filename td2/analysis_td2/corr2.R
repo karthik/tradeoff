828f36c1f6ec14126c1d2c6690602fe4fdb1b376
Karthik Ram <karthik.ram@gmail.com>
2012-09-30 16:05:32 -0700

    Removed trade off #2, adult survival vs fecundity since it is not working

--- td2/analysis_td2/corr2.R
+++
@@ -1,8 +0,0 @@
-# corr.R
-source('initialize2.R')
-total_cores <- 60
-t2_corr <- mclapply(corr_params , do_vd_tradeoff, mc.cores = total_cores, mc.preschedule = TRUE)
-fname4 <- generate_filename("../results_td2/t2_corr")
-save(t2_corr, file = fname4)
-
-# llply(sample(corr_params,1), do_vd_tradeoff)
--- td2/analysis_td2/juvshape2.R
+++
@@ -1,8 +0,0 @@
-# adshape.R
-source('initialize2.R')
-total_cores <- 60
-t2_adshape <- mclapply(vd_params, do_vd_tradeoff, mc.cores = total_cores, mc.preschedule = TRUE)
-fname3 <- generate_filename("../results_td2/t2_adshape")
-save(t2_adshape, file = fname3)
-
-# llply(sample(vd_params,1), do_vd_tradeoff)
